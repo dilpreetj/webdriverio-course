6eea78559b26dd74aae921959dc337efa3d0f9be
Automation Bro <automationbro@gmail.com>
2021-09-17 15:59:39 -0400

    comment oncomplete hook

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-const allure = require('allure-commandline')
+// const allure = require('allure-commandline')
 
 exports.config = {
     //
@@ -291,24 +291,24 @@ exports.config = {
      * @param {<Object>} results object containing test results
      */
     onComplete: function () {
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
+        // const reportError = new Error('Could not generate Allure report')
+        // const generation = allure(['generate', 'allure-results', '--clean'])
+        // return new Promise((resolve, reject) => {
+        //     const generationTimeout = setTimeout(
+        //         () => reject(reportError),
+        //         5000)
 
-            generation.on('exit', function (exitCode) {
-                clearTimeout(generationTimeout)
+        //     generation.on('exit', function (exitCode) {
+        //         clearTimeout(generationTimeout)
 
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
+        //         if (exitCode !== 0) {
+        //             return reject(reportError)
+        //         }
 
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
+        //         console.log('Allure report successfully generated')
+        //         resolve()
+        //     })
+        // })
     },
     /**
     * Gets executed when a refresh happens.
